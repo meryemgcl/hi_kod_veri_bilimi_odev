6e835fdbdfecae243e5a363e95cc7a77f879eb20
meryem <153759244+meryemgcl@users.noreply.github.com>
2025-01-27 22:53:32 +0300

    Add files via upload

---
+++ hafta2.1..py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+maas = int(input("lütfen maaş bilginizi giriniz: "))
+
+
+# In[9]:
+
+
+if maas <= 10000
+
+
+# In[10]:
+
+
+yeni_maas = maas - maas*0.05
+
+
+# In[11]:
+
+
+print ("yeni maasiniz:",yeni_maas)
+
+
+# In[12]:
+
+
+if 10000<maas<=25000:
+
+
+# In[13]:
+
+
+yeni_maas = maas - maas*0.10
+
+
+# In[14]:
+
+
+print ("yeni maaşınız: ",yeni_maas)
+
+
+# 
+
+# In[15]:
+
+
+if 25000 < maas <= 45000:
+
+
+# In[16]:
+
+
+yeni_maas = maas - maas*0.25
+
+
+# In[17]:
+
+
+print ("yeni maaşınız: ",yeni_maas)
+
+
+# In[18]:
+
+
+else :
+
+
+# In[19]:
+
+
+yeni_maas = maas - maas*0.30
+
+
+# In[20]:
+
+
+print ("yeni maaşınız: ",yeni_maas)
+
+
+# In[21]:
+
+
+print("yeni maaşınız: {}",format(yeni_maas))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
